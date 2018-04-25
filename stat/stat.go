99d7dd522e471148dc48d207625c66fe01c4f2ac
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-04-25 21:20:23 +0200

    Add omitempty to struct values

--- stat/stat.go
+++ stat/stat.go
@@ -3,8 +3,8 @@ package stat
 // Stat represents a stat value.
 type Stat struct {
 	ID        uint   `gorm:"primary_key" json:"-"`
-	Count     int64  `json:"count"`
-	Metric    string `json:"metric"`
-	Project   string `json:"project"`
-	Timestamp int64  `json:"timestamp"`
+	Count     int64  `json:"count,omitempty"`
+	Metric    string `json:"metric,omitempty"`
+	Project   string `json:"project,omitempty"`
+	Timestamp int64  `json:"timestamp,omitempty"`
 }
