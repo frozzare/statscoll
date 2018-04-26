b241d021dcf0dd6784a1159faa04ee7b17d31895
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-04-26 20:59:59 +0200

    Add value field and remove omitempty from count

--- stat/stat.go
+++ stat/stat.go
@@ -2,9 +2,10 @@ package stat
 
 // Stat represents a stat value.
 type Stat struct {
-	ID        uint   `gorm:"primary_key" json:"-"`
-	Count     int64  `json:"count,omitempty"`
-	Metric    string `json:"metric,omitempty"`
-	Project   string `json:"project,omitempty"`
-	Timestamp int64  `json:"timestamp,omitempty"`
+	ID        uint    `gorm:"primary_key" json:"-"`
+	Count     int64   `json:"count"`
+	Metric    string  `json:"metric,omitempty"`
+	Project   string  `json:"project,omitempty"`
+	Timestamp int64   `json:"timestamp,omitempty"`
+	Value     float64 `json:"value"`
 }
